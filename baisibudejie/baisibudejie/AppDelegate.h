2c82f47f5309dd2da1919f22e2ce8d1ec2f16a95
lvjun <18720087343@163.com>
2016-02-13 22:16:02 +0800

    初始化了项目

---
+++ baisibudejie/baisibudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisibudejie
+//
+//  Created by 吕俊 on 16/2/13.
+//  Copyright © 2016年 吕俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baisibudejie/baisibudejie/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  baisibudejie
+//
+//  Created by 吕俊 on 16/2/13.
+//  Copyright © 2016年 吕俊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
