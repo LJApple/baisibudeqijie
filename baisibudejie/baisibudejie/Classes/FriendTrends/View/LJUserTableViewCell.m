bf299853500a348237f941be5fb82ea94e2d8c34
lvjun <18720087343@163.com>
2016-02-17 14:21:04 +0800

    对一些文件进行了修改

--- baisibudejie/baisibudejie/Classes/FriendTrends/View/LJUserTableViewCell.m
+++ baisibudejie/baisibudejie/Classes/FriendTrends/View/LJUserTableViewCell.m
@@ -20,13 +20,11 @@
 @implementation LJUserTableViewCell
 
 - (void)awakeFromNib {
-    // Initialization code
+
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 - (void)setUsers:(LJUserRecomendModel *)users {
